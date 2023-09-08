6d0d0ce05f82e2e575c3034d70f7b7b21f7b8640
tinocolanna <pessoaltinocolanna@gmail.com>
2023-09-08 11:51:08 -0300

    .

--- Ed's/ed03/Exemplo0300.c
+++ Ed's/ed03/Exemplo0300.c
@@ -93,6 +93,233 @@ void method_03 ( void )
  // encerrar 
     IO_pause ( "Apertar ENTER para continuar" ); 
 } // end method_03 ( )
+/** 
+   Method_04 - Repeticao com teste no inicio e variacao. 
+ */ 
+void method_04 ( void ) 
+{ 
+ // definir dado 
+    int x = 0; 
+    int y = 0; 
+    int z = 0; 
+ 
+ // identificar 
+    IO_id ( "Method_04 - v0.0" ); 
+ 
+ // ler do teclado 
+    x = IO_readint ( "Entrar com uma quantidade: " );   
+ 
+ // repetir (x) vezes 
+ //        inicio    teste   variacao 
+    for ( y  = 1; y <= x; y = y + 1 ) 
+    { 
+     // ler valor do teclado 
+        z = IO_readint ( "Valor = " ); 
+     // mostrar valor atual 
+        IO_printf ( "%d. %d\n", y, z ); 
+    } // end for 
+ 
+ // encerrar 
+    IO_pause ( "Apertar ENTER para continuar" ); 
+} // end method_04 ( )
+/** 
+   Method_05 - Repeticao com teste no inicio e variacao. 
+ */ 
+void method_05 ( void ) 
+{ 
+ // definir dado 
+    int x = 0; 
+    int y = 0; 
+    int z = 0; 
+ 
+ // identificar 
+    IO_id ( "Method_05 - v0.0" ); 
+ 
+ // ler do teclado 
+    x = IO_readint ( "Entrar com uma quantidade: " ); 
+ 
+ // repetir (x) vezes 
+ //        inicio    teste   variacao 
+    for ( y  = x; y >= 1; y = y - 1 ) 
+    { 
+     // ler valor do teclado 
+        z = IO_readint ( "Valor = " ); 
+     // mostrar valor atual 
+        IO_printf ( "%d. %d\n", y, z ); 
+    } // end for 
+ 
+ // encerrar 
+    IO_pause ( "Apertar ENTER para continuar" ); 
+} // end method_05 ( )   
+/** 
+   Method_06 - Repeticao sobre cadeia de caracateres. 
+ */ 
+void method_06 ( void ) 
+{ 
+ // definir dado 
+    int x = 0; 
+    int y = 0; 
+     
+    chars palavra = IO_new_chars ( STR_SIZE ); 
+    int  tamanho   = 0; 
+ 
+ // identificar 
+    IO_id ( "Method_06 - v0.0" ); 
+ 
+ // ler do teclado 
+    palavra = IO_readstring ( "Entrar com uma palavra: " ); 
+ 
+ // repetir para cada letra 
+    tamanho = strlen ( palavra ) - 1; 
+//  OBS: A cadeia de caracteres iniciam suas posições em zero. 
+ 
+ //         inicio               teste   variacao 
+    for ( y  = tamanho; y >= 0; y = y - 1 ) 
+    { 
+     // mostrar valor atual 
+        IO_printf ( "%d: [%c]\n", y, palavra [y] ); 
+    } // end for 
+ 
+ // encerrar 
+    IO_pause ( "Apertar ENTER para continuar" ); 
+} // end method_06 ( ) 
+/** 
+   Method_07 - Repeticao sobre cadeia de caracateres. 
+ */ 
+void method_07 ( void ) 
+{ 
+ // definir dado 
+    int x = 0; 
+    int y = 0; 
+     
+    char palavra [STR_SIZE]; 
+    int  tamanho = 0; 
+ 
+ // identificar 
+    IO_id ( "Method07 - v0.0" ); 
+ 
+ // ler do teclado 
+    IO_printf ( "Entrar com uma palavra: " ); 
+    scanf  ( "%s", palavra ); getchar( ); 
+//  OBS: A cadeia de caracteres dispensa a indicacao de endereco (&) na leitura. 
+ 
+ 
+ 
+ // repetir para cada letra 
+    tamanho = strlen ( palavra ); 
+//  OBS: A cadeia de caracteres iniciam suas posições em zero. 
+ 
+ //         inicio        teste        variacao 
+    for ( y  = 0; y < tamanho; y = y + 1 ) 
+    { 
+     // mostrar valor atual 
+        IO_printf ( "%d: [%c]\n", y, palavra [y] ); 
+    } // end for 
+ 
+ // encerrar 
+    IO_pause ( "Apertar ENTER para continuar" ); 
+} // end method_07 ( ) 
+/** 
+   Method_08 - Repeticao com intervalos. 
+ */ 
+void method_08 ( void ) 
+{ 
+ // definir dado 
+    int inferior   = 0; 
+    int superior = 0; 
+    int x              = 0; 
+ 
+ // identificar 
+    IO_id ( "Method08 - v0.0" ); 
+ 
+ // ler do teclado 
+    inferior   = IO_readint ( "Limite inferior do intervalo: " ); 
+    superior = IO_readint ( "Limite superior do intervalo : " ); 
+ 
+ //                inicio             teste         variacao 
+    for ( x  = inferior; x <= superior; x = x + 1 ) 
+    { 
+     // mostrar valor atual 
+        IO_printf ( "%d\n", x ); 
+    } // end for 
+ 
+ // encerrar 
+    IO_pause ( "Apertar ENTER para continuar" ); 
+} // end method_08 
+/** 
+   Method_09 - Repeticao com intervalos. 
+ */ 
+void method_09 ( void ) 
+{ 
+ // definir dado 
+    double inferior   = 0; 
+    double superior = 0; 
+    double passo     = 0; 
+    double x             = 0; 
+ 
+ // identificar 
+    IO_id ( "Method_09 - v0.0" ); 
+ 
+ // ler do teclado 
+    inferior   = IO_readdouble ( "Limite inferior do intervalo : " ); 
+    superior = IO_readdouble ( "Limite superior do intervalo : " ); 
+    passo     = IO_readdouble ( "Variacao no intervalo (passo): " ); 
+ 
+ //                inicio               teste            variacao 
+    for ( x  = superior; x >= inferior; x = x - passo ) 
+    { 
+     // mostrar valor atual 
+        IO_printf ( "%lf\n", x ); 
+    } // end for 
+ 
+ // encerrar 
+    IO_pause ( "Apertar ENTER para continuar" ); 
+} // end method_09 ( ) 
+/** 
+   Method_10 - Repeticao com confirmacao. 
+ */ 
+void method_10 ( void ) 
+{ 
+ // definir dado 
+    double inferior   = 0; 
+    double superior = 0; 
+    double passo     = 0; 
+    double x             = 0; 
+ 
+ // identificar 
+    IO_id ( "Method10 - v0.0" ); 
+ 
+ // ler do teclado 
+    inferior = IO_readdouble ( "Limite inferior do intervalo : " ); 
+ 
+ 
+ 
+ 
+ 
+ // repetir ate' haver confirmacao de validade 
+    do 
+    { 
+        superior = IO_readdouble ( "Limite superior do intervalo: " ); 
+    } 
+    while ( inferior >= superior ); 
+ 
+ // repetir ate' haver confirmacao de validade 
+    do 
+    { 
+        passo = IO_readdouble ( "Variacao no intervalo (passo): " ); 
+    } 
+    while ( passo <= 0.0 ); 
+ 
+ //                inicio              teste              variacao 
+    for ( x  = inferior; x <= superior; x = x + passo ) 
+    { 
+     // mostrar valor atual 
+        IO_printf ( "%lf\n", x ); 
+    } // end for 
+ 
+ // encerrar 
+    IO_pause ( "Apertar ENTER para continuar" ); 
+} // end method_10 ( ) 
 int main ( )
 {
 // definir dado
@@ -105,7 +332,16 @@ int main ( )
  // ler do teclado
  IO_println ( "Opcoes" );
  IO_println ( "0 - parar" );
- IO_println ( "1 - repeticao com teste no inicio" );
+ IO_println ( "1 - Method_01" );
+ IO_println ( "2 - Method_02" );
+ IO_println ( "3 - Method_03" );
+ IO_println ( "4 - Method_04" );
+ IO_println ( "5 - Method_05" );
+ IO_println ( "6 - Method_06" );
+ IO_println ( "7 - Method_07" );
+ IO_println ( "8 - Method_08" );
+ IO_println ( "9 - Method_09" );
+ IO_println ( "10 - Method_10" );
  IO_println ( "" );
  x = IO_readint ( "Entrar com uma opcao: " );
  // testar valor
@@ -113,6 +349,15 @@ int main ( )
  {
  case 0: method_00 ( ); break;
  case 1: method_01 ( ); break;
+ case 2: method_02 ( ); break;
+ case 3: method_03 ( ); break;
+ case 4 : method_04 ( ); break;
+ case 5 : method_05 ( ); break;
+ case 6 : method_06 ( ); break;
+ case 7 : method_07 ( ); break;
+ case 8 : method_08 ( ); break;
+ case 9 : method_09 ( ); break;
+ case 10 : method_10 ( ); break;
  default:
  IO_pause ( IO_concat ( "Valor diferente das opcoes [0,1] (",
  IO_concat ( IO_toString_d ( x ), ")" ) ) );
